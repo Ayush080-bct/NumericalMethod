7f6993f21b3d4e81df96354b2e4ee371fbbe6aac
Ayush Sahu <080bct022@ioepc.edu.np>
2025-06-17 08:57:26 +0545

    add

--- Gausseli.py
+++ Gausseli.py
@@ -1,19 +1,35 @@
+
 import numpy as np
 
-n=int(input("Enter no of variables: "))
-A=[]
+n = int(input("Enter no of variables: "))
+A = []
 for i in range(n):
-    A.append(list(map(float,input(f'Enter the Element {i+1} row with {n+1} element and Press Enter: ').split(
-    ))))
-A=np.array(A)
-print("The augumented matrix A is: ")
+    A.append(list(map(float, input(f"Enter the Element {i+1} row with {n+1} element and Press Enter: ").split())))
+A = np.array(A, float)
+
+print("\nThe Augmented Matrix is:")
 print(np.matrix(A))
+
+
 for i in range(n):
-    pivotEl=np.argmax(np.abs(A[i:,i]))+i
-    print(A[pivotEl][i])
-    A[[i,pivotEl]]=A[[pivotEl,i]]
-    A[i]=A[i]/A[i,i]
-    print(A[i])
-   # for j in range(n): 
-    #    if j!=i:
-     #      A[j]=A[j]-A[j,i]*A[i]
+    
+    pivot = np.argmax(np.abs(A[i:, i])) + i
+
+    A[[i, pivot]] = A[[pivot, i]] 
+
+  
+    for j in range(i + 1, n):
+       
+        A[j, i:] = A[j, i:] - A[j,i]/A[i,i] * A[i, i:]
+
+print("\nUpper Triangular Matrix is:")
+print(np.matrix(A))
+
+x = np.zeros(n)
+for i in range(n-1,-1,-1):
+
+    x[i]=(A[i,-1]-np.sum(A[i,i+1:n]*x[i+1:n]))/A[i,i]
+  
+
+print("\nThe solution is:")
+print(np.matrix(x))
